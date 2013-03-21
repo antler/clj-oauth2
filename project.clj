7caad84f12476d1624ebbc321736daaac44f7c5f
Justin Glenn Smith <noisesmith@gmail.com>
2013-03-21 08:28:01 -0700

    fixed merge

--- project.clj
+++ project.clj
@@ -1,10 +1,7 @@
-(def dev-dependencies
-  '[[ring "0.3.11"]])
-
-(defproject clj-oauth2 "0.3.0"
+(defproject antler/clj-oauth2 "0.4.0-SNAPSHOT"
   :description "clj-http and ring middlewares for OAuth 2.0"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/data.json "0.1.1"]
+                 [org.clojure/data.json "0.2.1"]
                  [clj-http "0.3.2"]
                  [uri "1.1.0"]
                  [ring/ring-codec "1.0.0"]
