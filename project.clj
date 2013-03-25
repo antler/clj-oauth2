c02c8ea1c328d889c782889be282b05d5ea96b0e
Justin Glenn Smith <noisesmith@gmail.com>
2013-03-25 08:17:20 -0700

    updated deps

--- project.clj
+++ project.clj
@@ -1,14 +1,12 @@
 (defproject antler/clj-oauth2 "0.4.0-SNAPSHOT"
   :description "clj-http and ring middlewares for OAuth 2.0"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.2.1"]
                  [clj-http "0.3.2"]
                  [uri "1.1.0"]
                  [ring/ring-codec "1.0.0"]
                  [commons-codec/commons-codec "1.6"]]
   :exclusions   [org.clojure/clojure-contrib]
-  :dev-dependencies ~dev-dependencies
-  :profiles {:dev {:dependencies ~dev-dependencies}}
   :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"}
   :aot [clj-oauth2.OAuth2Exception
         clj-oauth2.OAuth2StateMismatchException])
